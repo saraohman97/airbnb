a455bb5d5b03e2780d9a7fc804d611b6e1caaf73
Sara Öhman <saraohman97@gmail.com>
2023-08-10 23:47:08 +0200

    Navbar, auth and backend.

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          {/* Reason behind not using <Toaster /> directly here is bc it needs at least one parent. */}
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
